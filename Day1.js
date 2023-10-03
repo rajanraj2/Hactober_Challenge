9ac83067cc0d9a23f4b4166eeea8a7c72eb83e2e
rajanraj2 <rajanraj253545@gmail.com>
2023-10-03 23:57:04 +0530

    Day 1 debug code add

---
+++ Day1.js
@@ -0,0 +1,13 @@
+// Link to problems:
+// https://codedamn.com/challenge/hacktoberfest-2023#start
+
+// Fix the Bug!
+
+const company = "Nike";
+const tagline = "Just do it";
+
+[company, tagline].forEach(
+	(el) => console.log(el)
+);
+
+// semicolon was not there after the company and tagline variable declaration
